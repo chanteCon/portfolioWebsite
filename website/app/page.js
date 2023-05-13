b35c7955b15f77e80394e86004711a60f70257ff
Chantelle <z5118127@ad.unsw.edu.au>
2023-05-13 23:30:06 +1000

    basic layout for homepage

--- website/app/page.js
+++ website/app/page.js
@@ -1,16 +1,21 @@
-import Image from "next/image"
+import { HomeImage } from "./components/HomeImage"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24" style = {{ backgroundColor: '#0E0F0E' }}>
-      <section
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24" style = {{ backgroundColor: '#0E0F0E' }}>
+      <section className = "flex flex-col flex-1 items-center justify-center  h-100 w-1/2 text-center pt-20"
       style = {{ backgroundColor: '#0E0F0E' }} >
+        <div className = "font-light">
+          <h1 className= "text-slate-200 text-2xl">I'm a</h1>
+          <h1 className = "text-orange-700 text-3xl">programmer</h1>
+        </div>
+        <p className = "text-slate-200 text-xs pt-8">
+           I’m a programmer with a wide skill set. I have experience with frontend and backend web projects, games, SaaS projects and more. I graduated from the University of New South Wales with a bachelor of Computer Science in 2023.
+        </p>
       </section>
-      <img
-        style = {{filter: 'contrast( 0.88)', opacity: '0.7' }}
+      <HomeImage
         src ={"/images/homePageImage.png"}
         alt="Picture of laptop"
-        priority
       />
     </main>
   )
