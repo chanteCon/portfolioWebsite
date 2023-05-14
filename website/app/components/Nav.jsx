5b9a67ed3da7b81582b44f48286af184821b10c0
Chantelle <z5118127@ad.unsw.edu.au>
2023-05-14 23:37:03 +1000

    git rid of errors when resizing the page

--- website/app/components/Nav.jsx
+++ website/app/components/Nav.jsx
@@ -1,47 +1,75 @@
-'use client';
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export const Nav = () => {
   const pathname = usePathname()
+  const [smallScreen, setSmallScreen] = useState(false)
+
+  const checkScreenSize = () => {
+    {console.log('resizing window')}
+    window.innerWidth <= 700 
+      ? setSmallScreen(true)
+      : setSmallScreen(false)
+      console.log(smallScreen)
+  }
+
+  useEffect(() => {
+    checkScreenSize()
+  }, []);
+
+  useEffect(() => {
+      window.addEventListener( 'resize', checkScreenSize)
+  }, []);
 
   const isActiveLink = (link) => {
-    if (link === pathname) return 'text-orange-700'
-    return 'text-slate-200' 
+    if (link === pathname) return 'text-amber-600 font-bold'
+    return 'text-neutral-400' 
   }
+  // if screen size small then collapse navbar
   return (
-    <div className= "w-full text-slate-200 text-xs pt-3 fixed flex justify-between px-10"> 
-      <section className = "flex space-x-3 items-center">
-        <Image
-          src ={"/images/me.jpeg"}
-          alt="Picture of laptop" 
-          className = "rounded-full"
-          width = {45}
-          height = {45} >
-        </Image>
-        <p>Chantelle Conlon Scoullar</p>
-      </section>
-      <section
-        className= "flex justify-end space-x-5 pt-2"
-        style = {{backgroundColor: '#1A1A1A'}}>
-          <Link 
-            href = '/' 
-            className = {isActiveLink('/')}
-          >
-            Home</Link>
-          <Link 
-            href = '/projects'
-            className = {isActiveLink('/projects')}
-          >
-            Recent Projects</Link>
-          <Link 
-            href = '/about'
-            className = {isActiveLink('/about')}
-          >
-            About Me</Link>
-          <div>Contact</div>
-        </section>
+    <div >
+      { smallScreen
+          ? (
+            <div> small screen </div>
+          )
+          : (
+            <div className= "w-full text-neutral-400 text-sm pt-3 fixed flex justify-between px-10"> 
+              <section className = "flex space-x-3 items-center">
+                <Image
+                  src ={"/images/me.jpeg"}
+                  alt="Picture of laptop" 
+                  className = "rounded-full"
+                  width = {45}
+                  height = {45} >
+                </Image>
+                <p>Chantelle Conlon Scoullar</p>
+              </section>
+              <section
+                className= "flex justify-end space-x-5 pt-2"
+                style = {{backgroundColor: '#1A1A1A'}}>
+                  <Link 
+                    href = '/' 
+                    className = {isActiveLink('/')}
+                  >
+                    Home</Link>
+                  <Link 
+                    href = '/projects'
+                    className = {isActiveLink('/projects')}
+                  >
+                    Recent Projects</Link>
+                  <Link 
+                    href = '/about'
+                    className = {isActiveLink('/about')}
+                  >
+                    About Me</Link>
+                  <div>Contact</div>
+                </section>
+            </div>
+        )
+      }
     </div>
   )
 }
--- website/app/page.js
+++ website/app/page.js
@@ -7,10 +7,10 @@ export default function Home() {
       <section className = "flex flex-col flex-1 items-center justify-center  h-100 w-1/2 text-center pt-40"
       style = {{ backgroundColor: '#1A1A1A' }} >
         <div className = "font-light">
-          <h1 className= "text-slate-200 text-2xl">I'm a</h1>
-          <h1 className = "text-orange-700 text-3xl">programmer</h1>
+          <h1 className= "text-neutral-400 text-2xl">I'm a</h1>
+          <h1 className = "text-amber-700 text-3xl">programmer</h1>
         </div>
-        <p className = "text-slate-200 text-sm pt-8">
+        <p className = "text-neutral-400 text-sm pt-8">
            I’m a programmer with a wide skill set. I have experience with frontend and backend web projects, games, SaaS projects and more. I graduated from the University of New South Wales with a bachelor of Computer Science in 2023.
         </p>
       </section>
