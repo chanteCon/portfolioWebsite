b5473c7e4d33339d6ea4f1bced62fe0f3bb83c80
Chantelle <z5118127@ad.unsw.edu.au>
2023-05-22 13:39:38 +1000

    fixed size of filter button for smaller screens

--- website/app/components/FilterBtn.jsx
+++ website/app/components/FilterBtn.jsx
@@ -3,19 +3,19 @@ import Image from "next/image"
 export const FilterBtn = ({ setFilterOn }) => {
   return (
     <button 
-      className = " px-4 text-neutral-100 rounded-2xl w-20 h-7" 
+      className = " px-4 text-neutral-100 rounded-2xl w-[50px] md:w-20 h-7" 
       style = {{ backgroundColor: '#F6A693' }}
       onClick = {setFilterOn}
       >
-        <div className = "flex flex-row space-x-2">
+        <div className = "flex flex-row md:space-x-2">
           <Image
-            className = "object-contain"
+            className = "object-contain ml-[-8px] md:ml-0"
             src = {"/images/icons/filter.png"}
             alt = ""
             width = {12}
             height = {12}
           />
-          <p className = "text-sm" >Filter</p>
+          <p className = "text-xs md:text-sm" >Filter</p>
         </div>
       </button>
   )
--- website/app/components/RecentProjTitle.jsx
+++ website/app/components/RecentProjTitle.jsx
@@ -1,6 +1,6 @@
 export const RecentProjTitle = ({activeFilter}) => {
   return (
-    <section className = "w-[62%] lg:w-[50%] flex justify-end">
+    <section className = "self-center">
       <div className = "flex flex-col">
         <h1 className= "text-neutral-300 text-2xl sm:text-3xl mt-[25px] font-extralight"> Recent Projects </h1>
         {
