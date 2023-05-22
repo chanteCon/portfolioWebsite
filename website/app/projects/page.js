8323c42dc193116e09be3ebb8d2f9e8f670cde66
Chantelle <z5118127@ad.unsw.edu.au>
2023-05-22 13:31:41 +1000

    fix jsutification of project cards

--- website/app/projects/page.js
+++ website/app/projects/page.js
@@ -10,7 +10,7 @@ import { BlurImage } from "../components/BlurImage"
 import Link from "next/link"
 
 export default function Projects() {
-  const filters = ["All", "Web", "Blockchain", "Games", "Released/Deployed", "Source code", "Blueprints", "Java", "JavaScript", "Python", "Solidity"]
+  const filters = ["All", "Web", "Blockchain", "Games", "Released/Deployed", "Source code", "Blueprints", "JavaScript", "Python", "Solidity"]
   const [showFilters, setShowFilters] = React.useState(false)
   const [activeFilter, setActiveFilter] = React.useState('All')
   const [hideImgCard, setHideImgCard] = React.useState('')
@@ -21,92 +21,95 @@ export default function Projects() {
     localStorage.getItem === null ? localStorage.setItem('projects filter', 'All') : setActiveFilter(window.localStorage.getItem('projects filter'))
   }, [showFilters]);
 
+
   return (
     <div className = "w-full flex flex-col align-center justify-center pt-[80px] pb-20">
         <RecentProjTitle activeFilter = {activeFilter}/>
         {/* Project cards container */}
-        <section className = "self-center pt-[100px] lg:w-[95%] xl:w-[65%] flex flex-wrap justify-center text-center gap-[20px]">
-            { 
-              displayProjects.map(project =>
-                hideImgCard === project.name 
-                ? 
-                <div 
-                  className = "h-[250px] w-[300px] md:h-[350px] md:w-[400px] overflow-hidden bg-[#262626] text-sm text-[#F2EAE8] relative pt-[40px]"
-                  key = {project.name}
-                  onMouseLeave = { () => {setHideImgCard('')} }
-                >
-                <p className= " text-md " >{project.name.toUpperCase()}</p>
-                  <p
-                    className= "p-[15px]" > {project.descriptions.projectsPage}
-                  </p>
-                  <div className = "flex flex-row flex-wrap justify-center gap-3">
-                  {
-                      project.links.map(link =>
-                      link.type !== "video" && 
-                      <a
-                        style = {{ lineHeight: '0.9' }}
-                        href = {link.link}
-                        target = "_blank"
-                        rel="noreferrer noopener"
-                        className = "h-[30px] w-[80px] rounded-2xl bg-[#F2EAE8] text-xs text-black flex px-[10px]"
-                      >
-                      <p className = "self-center" >{link.name}</p>
-                      <img 
-                        className = "w-[15px] h-[15px] self-center"
-                        src = '/images/icons/externalLinkBlack.png'
-                      />
-                    </a>)
-                  }
-                    <Link
-                          className = "h-[30px] w-[80px] rounded-2xl bg-[#F2EAE8] text-xs text-black"
-                          style = {{ lineHeight: '30px' }}
-                          href = {`/projects/${project.name}`}
-                          
-                    > MORE +
-                    </Link>
-                </div>
-                   <div
-                    className = "absolute top-[7px] right-[7px] w-[100px] h-[25px] bg-[#373737] rounded-3xl flex justify-between px-4"
-                  >
-                    <p
-                      className = "text-white text-xs self-center"
-                    >
-                      {project.skills[0].toUpperCase()}
-                    </p>
-                    <SmallTick />
-                  </div>
-                </div>
-                : 
-                <div 
-                  className = "h-[250px] w-[300px] md:h-[350px] md:w-[400px] overflow-hidden relative"
-                  key = {project.name}
-                  onMouseOver = { () => {setHideImgCard(project.name)} }
-                >
-                  <BlurImage
-                    className = "h-[250px] w-[300px] md:h-[350px] md:w-[400px] bg-cover bg-center"
-                    style = {{
-                      backgroundImage: `url(${project.images.mainImage})`,
-                    }}
-                    >
-                  </BlurImage>
-                  <p
-                    className = "relative bottom-[140px] md:bottom-[190px]"
-                  >
-                    {project.name.toUpperCase()}
-                  </p>
+        <section className = "self-center pt-[60px] lg:w-[95%] xl:w-[65%]" >
+            <div className = "flex flex-wrap text-center gap-[20px] justify-center" >
+              {
+                displayProjects.map(project =>
+                  hideImgCard === project.name
+                  ?
                   <div
-                    className = "absolute top-[7px] right-[7px] w-[115px] h-[30px] bg-[#373737] rounded-3xl flex justify-between px-4"
+                    className = "h-[250px] w-[300px] md:h-[350px] md:w-[400px] overflow-hidden bg-[#262626] text-sm md:text-lg text-[#F2EAE8] relative pt-[40px] p-[30px]"
+                    key = {project.name}
+                    onMouseLeave = { () => {setHideImgCard('')} }
                   >
+                  <p className= " text-md " >{project.name.toUpperCase()}</p>
                     <p
-                      className = "text-white text-sm self-center"
-                    >
-                      {project.skills[0].toUpperCase()}
+                      className= "md:p-[30px] pt-[15px]" > {project.descriptions.projectsPage}
                     </p>
-                    <SmallTick />
+                    <div className = "flex flex-row flex-wrap justify-center md:gap-7 gap-3 pt-[15px] md:pt-0">
+                    {
+                        project.links.map(link =>
+                        link.type !== "video" &&
+                        <a
+                          style = {{ lineHeight: '0.9' }}
+                          href = {link.link}
+                          target = "_blank"
+                          rel="noreferrer noopener"
+                          className = "h-[30px] w-[80px] rounded-2xl bg-[#F2EAE8] text-xs text-black flex px-[10px]"
+                        >
+                        <p className = "self-center" >{link.name}</p>
+                        <img
+                          className = "w-[15px] h-[15px] self-center"
+                          src = '/images/icons/externalLinkBlack.png'
+                        />
+                      </a>)
+                    }
+                      <Link
+                            className = "h-[30px] w-[80px] rounded-2xl bg-[#F2EAE8] text-xs text-black"
+                            style = {{ lineHeight: '30px' }}
+                            href = {`/projects/${project.name}`}
+              
+                      > MORE +
+                      </Link>
                   </div>
-              </div>
-              )
-            }
+                     <div
+                      className = "absolute top-[7px] right-[7px] w-[100px] h-[25px] bg-[#373737] rounded-3xl flex justify-between px-4"
+                    >
+                      <p
+                        className = "text-white text-xs self-center"
+                      >
+                        {project.skills[0].toUpperCase()}
+                      </p>
+                      <SmallTick />
+                    </div>
+                  </div>
+                  :
+                  <div
+                    className = "h-[250px] w-[300px] md:h-[350px] md:w-[400px] overflow-hidden relative"
+                    key = {project.name}
+                    onMouseOver = { () => {setHideImgCard(project.name)} }
+                  >
+                    <BlurImage
+                      className = "h-[250px] w-[300px] md:h-[350px] md:w-[400px] bg-cover bg-center"
+                      style = {{
+                        backgroundImage: `url(${project.images.mainImage})`,
+                      }}
+                      >
+                    </BlurImage>
+                    <p
+                      className = "md:text-3xl relative bottom-[140px] md:bottom-[190px]"
+                    >
+                      {project.name.toUpperCase()}
+                    </p>
+                    <div
+                      className = "absolute top-[7px] right-[7px] w-[115px] h-[30px] bg-[#373737] rounded-3xl flex justify-between px-4"
+                    >
+                      <p
+                        className = "text-white text-sm self-center"
+                      >
+                        {project.skills[0].toUpperCase()}
+                      </p>
+                      <SmallTick />
+                    </div>
+                </div>
+                )
+              }
+            </div>
         </section>
 
       {/* Buttons for filter and filter list */}
