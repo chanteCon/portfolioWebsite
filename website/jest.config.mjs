8dc2b28df759727650cc0cbed66197af2a66a72c
Chante <100500922+chanteCon@users.noreply.github.com>
2023-05-25 19:37:00 +1000

    Update jest.config.mjs

--- website/jest.config.mjs
+++ website/jest.config.mjs
@@ -12,6 +12,11 @@ const config = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
  
   testEnvironment: 'jest-environment-jsdom',
+   coverageThreshold: {
+        global: {
+            lines: 90,
+        },
+    },
 };
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
