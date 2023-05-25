4f13cff787d122ab7e8e4ad14509f100c04a0729
Chantelle <z5118127@ad.unsw.edu.au>
2023-05-25 17:40:53 +1000

    test for filter projects by category button

--- website/app/components/ProjectsByCategory.jsx
+++ website/app/components/ProjectsByCategory.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export const ProjectsByCategory = ({ filter }) => {
   return (
     <Link 
+      data-testid = "projectCategory"
       className= "w-[110px] h-[30px] bg-[#F6A693] rounded-3xl hover:opacity-80  flex justify-center items-center"
       href={{ pathname: '/projects', query: { filter: filter } }}
       >
