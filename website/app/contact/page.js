52f8c93ed137b4af6a223cd2ea1580f0cccb9a94
Chantelle <z5118127@ad.unsw.edu.au>
2023-06-05 13:46:02 +1000

    stop error message going outside of background for mobile creens

--- website/app/contact/page.js
+++ website/app/contact/page.js
@@ -16,10 +16,10 @@ export default function Contact() {
         <p className = "text-lg" >I would love to hear from you</p>
       </section>
       <section className = "justify-center w-full flex flex-wrap-reverse">
-        <div className = "w-[300px] md:w-[400px] lg:w-[500px] xl:w-[550px] h-[350px] md:h-[450px] lg:h-[600px] bg-[#373737] flex flex-col pt-[20px] lg:pt-[70px] md:pt-[50px] items-center">
+        <div className = "justify-center w-[300px] sm:w-[400px] lg:w-[500px] xl:w-[550px] h-[500px] md:h-[600px] bg-[#373737] flex flex-col pt-[20px] lg:pt-[70px] md:pt-[50px] items-center">
           <ContactForm />
         </div>
-        <div className = "w-[300px] md:w-[400px] lg:w-[500px] xl:w-[550px] h-[350px] md:h-[450px] lg:h-[600px] bg-[#262626] text-left p-[30px] md:p-[100px] flex flex-col gap-[50px]">
+        <div className = "justify-center w-[300px] sm:w-[400px] lg:w-[500px] xl:w-[550px] h-[500px] md:h-[600px] bg-[#262626] text-left p-[30px] sm:p-[100px] flex flex-col gap-[50px]">
           <div className = "self-start flex flex-col items-center">
             <p className = "lg:text-lg" >Chantelle Conlon Scoullar</p>
             <img 
